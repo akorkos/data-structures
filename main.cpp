c8b53bd09f9bb5735305339c6fa13f0a1a612cc0
Alexandros <74056174+akorkos@users.noreply.github.com>
2021-05-08 00:11:31 +0300

    Add files via upload
    temp file gia read apo to file kai aou

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "Student.h"
+
+using namespace std;
+
+int main()
+{
+    char s[30];
+    cin>>s;
+    Student a,b;
+    if (readStudent(s,a))
+    {
+        cout<<a<<endl;
+        cin>>b;
+        cin>>s;
+        if (writeToStudent(s,a>b?a:b))
+            cout<<"Ok"<<endl;
+        else
+            cout<<"Error writing to file"<<endl;
+    }
+    else
+        cout<<"Error reading from file"<<endl;
+    return 0;
+}
