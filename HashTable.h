68d7c561d2c761f604510025c1d21b6d9cbba36e
akorkos <alexkork@csd.auth.gr>
2021-05-27 19:48:39 +0300

    Είναι τζιτζι, δεν χρειάζονται άλλες αλλαγές.

--- HashTable.h
+++ HashTable.h
@@ -1,29 +1,25 @@
 #ifndef HASHMAP_H
 #define HASHMAP_H
+#include <iostream>
 #include <string>
 
 using namespace std;
 
 struct HashNode{
     string word;
-    int address;
-    int appearances;
+    int address, appearances;
 };
 
-const int size=100000;
+const int size = 100000;
 
 class HashTable{
+private:
+    HashNode **root;
+    int findAddress(string key);
 public:
     HashTable();
     ~HashTable();
-
     void insert(string key);
-    HashNode* search(string key);
-    HashNode **getRoot(){return root;}
-
-private:
-    int findAddress(string key);
-    HashNode **root;
+    int search(string key);
 };
-
-#endif //DOMES_DEDOMENWN_2021_HASHMAP_H
+#endif
